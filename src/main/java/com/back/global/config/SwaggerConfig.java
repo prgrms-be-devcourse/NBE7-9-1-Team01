48aca9e35600c62abbf1d4ea47734a53744d6464
myoungjinseo <sih5673@gmail.com>
2025-09-23 16:50:39 +0900

    feat: 스웨거 설정

---
+++ src/main/java/com/back/global/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.back.global.config;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Caffe Menu API 명세서")
+                        .version("v0.0.1")
+                        .description("카페 메뉴 관리 애플리케이션입니다."));
+    }
+
+
+}
