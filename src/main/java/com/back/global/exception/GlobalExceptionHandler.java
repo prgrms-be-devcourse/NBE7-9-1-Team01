9be9443f7c2abb2cd547c15b1ddcfa1700e72967
myoungjinseo <sih5673@gmail.com>
2025-09-24 12:04:24 +0900

    feat: 예외처리

---
+++ src/main/java/com/back/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,52 @@
+package com.back.global.exception;
+
+import com.back.global.dto.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    // 커스텀 예외 처리
+    @ExceptionHandler(ErrorException.class)
+    protected ResponseEntity<ApiResponse<?>> handleCustomException(ErrorException e) {
+        log.error("ErrorException: {} - {}", e.getErrorCode().name(), e.getMessage(), e);
+        return new ResponseEntity<>(
+                ApiResponse.fail(e)
+                , e.getErrorCode().getHttpStatus());
+    }
+
+    // @Valid 유효성 검사 실패 시 발생하는 예외 처리
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<?>> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .filter(error -> error instanceof FieldError)
+                .map(error -> (FieldError) error)
+                .map(error -> error.getField() + "-" + error.getCode() + "-" + error.getDefaultMessage())
+                .sorted(Comparator.comparing(String::toString))
+                .collect(Collectors.joining("\n"));
+        log.warn("MethodArgumentNotValidException {}", e.getMessage());
+        return new ResponseEntity<>(
+                ApiResponse.fail(HttpStatus.BAD_REQUEST,message)
+                , HttpStatus.BAD_REQUEST);
+    }
+
+    // 그 외 모든 예외 처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<?>> handleAllException(final Exception e) {
+        log.error("handleAllException {}", e.getMessage(), e);
+        return new ResponseEntity<>(
+                ApiResponse.fail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage()),
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
