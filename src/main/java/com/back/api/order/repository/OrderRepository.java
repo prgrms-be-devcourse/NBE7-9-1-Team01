cd8a3dd5de42dbdd54d4f84c2e47277043eb0e90
qivvoon <gangjiwon70@gmail.com>
2025-09-24 15:22:56 +0900

    feat: 주문 스케줄러 구현

---
+++ src/main/java/com/back/api/order/repository/OrderRepository.java
@@ -0,0 +1,23 @@
+package com.back.api.order.repository;
+
+import com.back.domain.order.entity.Order;
+import com.back.domain.order.entity.OrderStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Order o SET o.orderStatus = :shipped " +
+            "WHERE o.orderStatus = :processing " +
+            "AND o.createDate BETWEEN :start AND :end")
+    int updateStatusToShipped(
+            @Param("processing") OrderStatus processing,
+            @Param("shipped") OrderStatus shipped,
+            @Param("start") LocalDateTime start,
+            @Param("end") LocalDateTime end);
+}
