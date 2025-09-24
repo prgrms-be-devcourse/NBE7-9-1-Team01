a3910df2bbe9d47424d8bc2d85c60fae74477392
myoungjinseo <sih5673@gmail.com>
2025-09-24 12:04:40 +0900

    feat: ApiResponse

---
+++ src/main/java/com/back/global/dto/response/ApiResponse.java
@@ -0,0 +1,32 @@
+package com.back.global.dto.response;
+
+import com.back.global.exception.ErrorCode;
+import com.back.global.exception.ErrorException;
+import org.springframework.http.HttpStatus;
+
+public record ApiResponse<T>(
+        HttpStatus status,
+        String message,
+        T data
+) {
+    public static <T> ApiResponse<T> ok( String message, T data) {
+        return new ApiResponse<>(HttpStatus.OK, message, data);
+    }
+
+    public static ApiResponse<?> fail(ErrorException errorException) {
+        ErrorCode errorCode = errorException.getErrorCode();
+        return new ApiResponse<>(
+                errorCode.getHttpStatus(),
+                errorCode.getMessage(),
+                null
+        );
+    }
+
+    public static ApiResponse<?> fail(HttpStatus status, String message) {
+        return new ApiResponse<>(
+                status,
+                message,
+                null
+        );
+    }
+}
