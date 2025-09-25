d3685dff13b21819269967fd7321472516193d9e
myoungjinseo <sih5673@gmail.com>
2025-09-25 09:59:07 +0900

    test: 제품 수정 API 정상 테스트 및 실패 테스트

--- src/test/java/com/back/api/product/controller/ProductControllerTest.java
+++ src/test/java/com/back/api/product/controller/ProductControllerTest.java
@@ -1,6 +1,7 @@
 package com.back.api.product.controller;
 
-import com.back.api.product.service.ProductService;
+import com.back.api.product.dto.request.ProductUpdateRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -13,9 +14,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,9 +28,12 @@ class ProductControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
 
     @Nested
-    @DisplayName("제품 API 수정")
+    @DisplayName("제품 수정 API")
     class t1 {
         @Test
         @DisplayName("정상 작동")
@@ -36,23 +41,75 @@ class ProductControllerTest {
 
             // given
             long targetProductId = 1;
-            String description = "콜롬비아 커피 원두 입니다.";
+            ProductUpdateRequest request = new ProductUpdateRequest("콜롬비아 커피 원두 입니다.");
 
             // when
-            ResultActions result = mockMvc.perform(
-                    put("api/product/%d".formatted(targetProductId))
+            ResultActions resultActions = mockMvc.perform(
+                    patch("/api/products/%d".formatted(targetProductId))
                             .accept(MediaType.APPLICATION_JSON)
                             .contentType(MediaType.APPLICATION_JSON)
-                            .content(description)
+                            .content(objectMapper.writeValueAsString(request))
             );
 
             // then
-            result
+            resultActions
                     .andExpect(handler().handlerType(ProductController.class))
                     .andExpect(handler().methodName("update"))
                     .andExpect(status().isOk())
                     .andExpect(jsonPath("$.data.id").value(1))
                     .andExpect(jsonPath("$.data.description").value("콜롬비아 커피 원두 입니다."))
+                    .andDo(print());
+        }
+
+        @Test
+        @DisplayName("valid 문제")
+        void fail1() throws Exception {
+
+            // given
+            long targetProductId = 1;
+            ProductUpdateRequest request = new ProductUpdateRequest(null);
+
+            // when
+            ResultActions resultActions = mockMvc.perform(
+                    patch("/api/products/%d".formatted(targetProductId))
+                            .accept(MediaType.APPLICATION_JSON)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request))
+            );
+
+            // then
+            resultActions
+                    .andExpect(handler().handlerType(ProductController.class))
+                    .andExpect(handler().methodName("update"))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                    .andExpect(jsonPath("$.message").value("설명이 비어있습니다."))
+                    .andDo(print());
+        }
+
+        @Test
+        @DisplayName("product가 존재하지 않을 때")
+        void fail2() throws Exception {
+
+            // given
+            long targetProductId = 10000;
+            ProductUpdateRequest request = new ProductUpdateRequest("콜롬비아 커피 원두 입니다.");
+
+            // when
+            ResultActions resultActions = mockMvc.perform(
+                    patch("/api/products/%d".formatted(targetProductId))
+                            .accept(MediaType.APPLICATION_JSON)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request))
+            );
+
+            // then
+            resultActions
+                    .andExpect(handler().handlerType(ProductController.class))
+                    .andExpect(handler().methodName("update"))
+                    .andExpect(status().isNotFound())
+                    .andExpect(jsonPath("$.status").value("NOT_FOUND"))
+                    .andExpect(jsonPath("$.message").value("제품을 찾을 수 없습니다."))
                     .andDo(print());
         }
     }
