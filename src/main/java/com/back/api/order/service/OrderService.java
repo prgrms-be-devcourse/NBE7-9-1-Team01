a2533d8ab1fe0d0fc311b856d89c46d79c67080a
qivvoon <gangjiwon70@gmail.com>
2025-09-25 14:52:48 +0900

    refactor: 스케줄 시간 포맷 변경 (nnn -> ss)

--- src/main/java/com/back/api/order/service/OrderService.java
+++ src/main/java/com/back/api/order/service/OrderService.java
@@ -18,8 +18,8 @@ public class OrderService {
     public int dailyOrderProcess() {
         LocalDateTime now = LocalDateTime.now();
         LocalDateTime yesterday = now.minusDays(1);
-        LocalDateTime start = yesterday.withHour(14).withMinute(0).withSecond(0).withNano(0);
-        LocalDateTime end = now.withHour(14).withMinute(0).withSecond(0).withNano(0);
+        LocalDateTime start = yesterday.withHour(14).withMinute(0).withSecond(0);
+        LocalDateTime end = now.withHour(14).withMinute(0).withSecond(0);
 
         // PROCESSING -> SHIPPED 처리 및 처리 완료된 주문 개수 반환
         int processComplete = orderRepository.updateStatusToShipped(OrderStatus.PROCESSING, OrderStatus.SHIPPED, start, end);
