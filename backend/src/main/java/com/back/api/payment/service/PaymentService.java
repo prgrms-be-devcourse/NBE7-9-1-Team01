6fadfe03f0b4f255fb2f9f08ea8ab3506afe9370
myoungjinseo <sih5673@gmail.com>
2025-09-25 16:06:22 +0900

    refactor: 프로젝트 구조 변경

---
+++ backend/src/main/java/com/back/api/payment/service/PaymentService.java
@@ -0,0 +1,12 @@
+package com.back.api.payment.service;
+
+import com.back.domain.payment.repository.PaymentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+
+    private final PaymentRepository paymentRepository;
+}
