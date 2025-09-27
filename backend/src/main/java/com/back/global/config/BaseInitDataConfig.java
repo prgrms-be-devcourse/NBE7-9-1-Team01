ff0b43da0f479256bf526d50a3af27733908592b
qivvoon <gangjiwon70@gmail.com>
2025-09-27 16:04:48 +0900

    feat: DB에 Member 1개 저장

--- backend/src/main/java/com/back/global/config/BaseInitDataConfig.java
+++ backend/src/main/java/com/back/global/config/BaseInitDataConfig.java
@@ -1,8 +1,10 @@
 package com.back.global.config;
 
+import com.back.api.member.service.MemberService;
 import com.back.api.product.dto.request.ProductCreateRequest;
 import com.back.api.product.service.ProductService;
-import com.back.domain.product.repository.ProductRepository;
+import com.back.domain.member.entity.Member;
+import com.back.domain.member.entity.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -15,11 +17,13 @@ import java.util.List;
 public class BaseInitDataConfig {
 
     private final ProductService productService;
+    private final MemberService memberService;
 
     @Bean
     ApplicationRunner initData() {
         return args -> {
             work();
+            work2();
         };
     }
 
@@ -35,5 +39,14 @@ public class BaseInitDataConfig {
 
         productService.saveAll(requests);
 
+    }
+
+    private void work2() {
+        if (memberService.count() > 0)
+            return;
+
+        Member member = new Member("test@test.com", "test", "test", "test", Role.ROLE_USER);
+
+        memberService.save(member);
     }
 }
