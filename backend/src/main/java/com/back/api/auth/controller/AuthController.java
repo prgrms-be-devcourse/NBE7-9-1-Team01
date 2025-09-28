780c7d732091c796e28062ac75aed25b480f2f11
tobbot16 <tobbot16@naver.com>
2025-09-28 13:52:34 +0900

    feat: controller 구현

--- backend/src/main/java/com/back/api/auth/controller/AuthController.java
+++ backend/src/main/java/com/back/api/auth/controller/AuthController.java
@@ -1,9 +1,28 @@
 package com.back.api.auth.controller;
 
+import com.back.api.auth.dto.request.AuthRequest;
+import com.back.api.auth.dto.response.AuthResponse;
+import com.back.api.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/auth")
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public AuthResponse login (@RequestBody AuthRequest authRequest){
+        return authService.login(authRequest);
+    }
+
+    @PostMapping("/logout")
+    public AuthResponse logout (@RequestBody AuthRequest authRequest){
+        return authService.logout(authRequest);
+    }
 }
