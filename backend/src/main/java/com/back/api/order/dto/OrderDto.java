db5590abcab99baaf3c1a1fc7abd7a3af6bb58a8
qivvoon <gangjiwon70@gmail.com>
2025-09-28 18:54:09 +0900

    refactor: orderDate 타입 LocalDate -> LocalDateTime 변경

--- backend/src/main/java/com/back/api/order/dto/OrderDto.java
+++ backend/src/main/java/com/back/api/order/dto/OrderDto.java
@@ -5,13 +5,14 @@ import com.back.domain.order.entity.Order;
 import com.back.domain.order.entity.OrderProduct;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public record OrderDto(
         Long orderId,
         String memberEmail,
-        LocalDate orderDate,
+        LocalDateTime orderDate,
         List<OrderProductDto> orderProducts
 ) {
     // Order 엔티티를 Dto로 변환하는 생성자
