76051443f48b844766960315a2da2368ac2e3c1f
myoungjinseo <sih5673@gmail.com>
2025-09-29 13:50:33 +0900

    feat: 스웨거 설정

--- backend/src/main/java/com/back/api/order/controller/AdminOrderController.java
+++ backend/src/main/java/com/back/api/order/controller/AdminOrderController.java
@@ -8,6 +8,7 @@ import com.back.global.dto.requset.PageRequestDto;
 import com.back.global.dto.response.ApiResponse;
 import com.back.global.dto.response.PageResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -32,9 +33,13 @@ public class AdminOrderController {
         return ApiResponse.ok("주문 통계 조회 성공",responses);
     }
 
+    @Operation(summary = "판매량 조회 API", description = "판매량을 조회하는 API입니다.")
     @GetMapping("/sales")
     public ApiResponse<List<SalesResponse>> getSales(
+            @Parameter(description = "조회 시작 날짜", example = "2023-01-01")
             @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
+
+            @Parameter(description = "조회 종료 날짜", example = "2023-01-31")
             @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate
     ) {
         SalesRequest request = new SalesRequest(startDate, endDate);
--- backend/src/main/java/com/back/api/order/dto/response/SalesResponse.java
+++ backend/src/main/java/com/back/api/order/dto/response/SalesResponse.java
@@ -1,7 +1,12 @@
 package com.back.api.order.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "SalesResponse", description = "판매량 조회 응답 dto")
 public record SalesResponse(
+    @Schema(description = "제품 이름", example = "Columbia Narino")
     String productName,
+    @Schema(description = "제품의 총 판매량", example = "100")
     Long totalQuantity
 ) {
 }
