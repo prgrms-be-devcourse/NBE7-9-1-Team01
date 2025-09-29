834d4743ac881c37a577a575d22ab466048bd024
tobbot16 <tobbot16@naver.com>
2025-09-29 16:51:49 +0900

    fix: 번호수정

--- backend/src/main/java/com/back/api/auth/controller/AuthController.java
+++ backend/src/main/java/com/back/api/auth/controller/AuthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
-@Tag(name = "4. [보안] ", description = "인증 관련 API입니다.")
+@Tag(name = "5. [보안] ", description = "인증 관련 API입니다.")
 public class AuthController {
 
     private final AuthService authService;
